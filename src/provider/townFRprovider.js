8aff787add4902a1659d2663dcbf71c02360fccb
MartinJeanne <martinjeanne.dev@gmail.com>
2024-05-23 21:30:24 +0200

    return weather on some cities

--- src/provider/townFRprovider.js
+++ src/provider/townFRprovider.js
@@ -1,41 +1,47 @@
+const { response } = require('express');
 const redis = require('../redisClient');
 const google = require('googlethis');
 
 const apiGeo = 'https://geo.api.gouv.fr';
 
 exports.getChoice = async function (choiceType) {
-    let allChoices = await redis.getJSON(choiceType);
+    let allCities = await redis.getJSON(choiceType);
 
-    if (!allChoices) {
-        allChoices = await fetch(`${apiGeo}/departements/14/communes`)
+    if (!allCities) {
+        allCities = await fetch(`${apiGeo}/departements/14/communes`)
             .then(response => response.json())
             .catch(console.error);
 
         // Remove the villages
-        allChoices = allChoices.filter((c) => c.population > 2000);
-        redis.saveJSON(choiceType, allChoices);
+        allCities = allCities.filter((c) => c.population > 2000);
+        redis.saveJSON(choiceType, allCities);
     }
 
-    const rawChoice1 = getRandomElement(allChoices);
-    const indexToDelete = allChoices.indexOf(rawChoice1);
-    allChoices.splice(indexToDelete, 1);
-    const rawChoice2 = getRandomElement(allChoices);
+    const city1 = getRandomElement(allCities);
+    const indexToDelete = allCities.indexOf(city1);
+    allCities.splice(indexToDelete, 1);
+    const city2 = getRandomElement(allCities);
 
-    const choices = toChoices(rawChoice1, rawChoice2);
-
-    const searchOptions = {
-        safe: false, // Safe Search
-        parse_ads: false, // If set to true sponsored results will be parsed
-    }
+    const choices = toChoices(city1, city2);
 
     for (let i = 0; i < choices.length; i++) {
-        // Image Search
-        const images = await google.image(`${choices[i].name} landscape`, searchOptions);
-
-        if (images) choices[i].image = images[0];
+        choices[i].metadata.weather = await getWeather(choices[i].name);
     }
 
     return choices;
+}
+
+async function getWeather(name) {
+    const cityName = name.replace(' ', '+');
+
+    const weatherRes = await fetch(`http://wttr.in/${cityName}?format=3`)
+        .catch(console.error);
+
+    if (weatherRes.ok) {
+        let weather = await weatherRes.text();
+        weather = weather.slice(0, -1);
+        return weather;
+    }
 }
 
 function getRandomElement(arr) {
@@ -43,17 +49,31 @@ function getRandomElement(arr) {
     return arr[randomIndex];
 }
 
+async function getImage(choice) {
+    const searchOptions = {
+        safe: false, // Safe Search
+        parse_ads: false, // If set to true sponsored results will be parsed
+    }
+
+    const images = await google.image(`${choice.name}`, searchOptions);
+    if (images) return images[0];
+}
+
+
+
 function toChoices(rawChoice1, rawChoice2) {
     const choice1 = {
         name: rawChoice1.nom,
         data: rawChoice1.population,
         image: '',
+        metadata: {},
         isCorrectAwnser: false,
     }
     const choice2 = {
         name: rawChoice2.nom,
         data: rawChoice2.population,
         image: '',
+        metadata: {},
         isCorrectAwnser: false,
     }
 
